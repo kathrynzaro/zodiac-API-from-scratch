7cf254ab9e8106e7921688c025171826dcd70d5a
kathrynzaro <zarokathryn@gmail.com>
2022-07-25 17:23:58 -0700

    test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,32 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const { zodiacs } = require('../data/zodiacs');
+
+describe('zodiacs routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/zodiacs should return a list of zodiacs', async () => {
+    const res = await request(app).get('/zodiacs');
+    const expected = zodiacs.map((zodiac) => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(res.body).toEqual(expected);
   });
+
+  it('/zodiacs/:id should return zodiac detail', async () => {
+    const res = await request(app).get('/zodiacs/1');
+    const felix = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    };
+    expect(res.body).toEqual(felix);
+  });
+
   afterAll(() => {
     pool.end();
   });
